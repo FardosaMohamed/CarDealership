4df470a99ec52065f560ac34de95dc9838620ba9
fardosamohamed <Fardosa.Mohamed2@seattlecolleges.edu>
2025-05-09 14:53:25 -0700

    Phase 1 complete

--- src/Dealership.java
+++ src/Dealership.java
@@ -1,4 +1,100 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    Dealership(String name,String address,String phone){
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<Vehicle>();
+    }
+    public String toString() {
+        return String.format(
+                "DEALERSHIP NAME: '%s' #CARS: %d\n",
+                this.name, this.inventory.size()
+        );
+    }
+
+    public List<Vehicle> getVehiclesByPrice(double min,double max) {
+        return inventory;
+    }
+
+    public List<Vehicle>  getVehiclesByMakeModel(String make, String model){
+        return inventory;
+    }
+
+    public List<Vehicle>  getVehiclesByYear(int min,int max){
+        return inventory;
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color) {
+        return inventory;
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int min, int max) {
+        return inventory;
+    }
+
+    public List<Vehicle> getVehiclesByType(String type) {
+        return inventory;
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        return inventory;
+    }
+
+
+    void addVehicle(Vehicle v){
+        inventory.add(v);
+    }
+
+    void removeVehicle(Vehicle v) {
+        inventory.remove(v);
+    }
+
+
+
+   // Getters/Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ArrayList<Vehicle> inventory) {
+        this.inventory = inventory;
+    }
+
+
+    public static void main(String[] args) {
+    }
+
+
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
-
 public class Vehicle {
+    int vin;
+    int year;
+    String make;
+    String model;
+    String vehicleType;
+    String color;
+    int odometer;
+    double price;
+
+    Vehicle( int vin,
+    int year,
+    String make,
+    String model,
+    String vehicleType,
+    String color,
+    int odometer,
+    double price){
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model= model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
+
+    public String toString() {
+        return String.format("""
+                VIN: '%d'
+                YEAR: '%d'
+                MAKE: '%s'
+                MODEL: '%s'
+                COLOR: '%s'
+                TYPE: '%s'
+                PRICE: '$%.2f'
+                MILES: '%d'
+                """,
+                this.vin, this.year, this.make, this.model, this.color, this.vehicleType, this.price, this.odometer);
+    }
 }
